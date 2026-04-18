dc04fb40e38df63bd715249d79d1e99b3aea47ef
Akhila-1703 <akhiladhachepally@gmail.com>
2026-04-18 19:42:25 +0530

    Week9 & Week10 backend+frontend

---
+++ Week9-Week10/frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+
+  return (
+    <div className="bg-gray-800 text-white text-center p-4">
+
+      <p>© 2026 Blog Platform</p>
+
+    </div>
+  )
+
+}
+
+export default Footer
